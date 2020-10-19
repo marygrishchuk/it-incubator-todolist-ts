c8cd9d4e7cca2d9c61ed10800171e6b5fb6503b3
Maria Grishchuk <mary.grishchuk1@gmail.com>
2020-10-20 01:30:52 +0300

    Updates

---
+++ precache-manifest.4251b154916ee479dbc8f14b7b4b9217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd1461b1d4d0cbb1f52f6033d946c52",
+    "url": "/it-incubator-todolist-ts/index.html"
+  },
+  {
+    "revision": "e893fa505bbe5e909a9e",
+    "url": "/it-incubator-todolist-ts/static/css/main.99052939.chunk.css"
+  },
+  {
+    "revision": "a73737db3ea66ca28a54",
+    "url": "/it-incubator-todolist-ts/static/js/2.c25cb00b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/it-incubator-todolist-ts/static/js/2.c25cb00b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e893fa505bbe5e909a9e",
+    "url": "/it-incubator-todolist-ts/static/js/main.f6d4d5ee.chunk.js"
+  },
+  {
+    "revision": "1ab7487ce92f6fb728b6",
+    "url": "/it-incubator-todolist-ts/static/js/runtime-main.f8216a61.js"
+  }
+]);
